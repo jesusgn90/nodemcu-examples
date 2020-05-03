25aa198f53cd7b0957e4d7c9708d581abf43f337
Jesús Ángel <jesusgonzaleznovez@gmail.com>
2020-05-03 13:06:10 +0200

    Update mqtt-wifi-dht11.ino

--- MQTT-WIFI-DHT11/mqtt-wifi-dht11.ino
+++ MQTT-WIFI-DHT11/mqtt-wifi-dht11.ino
@@ -14,14 +14,14 @@ DHT DHT(DHT11_PIN, DHT11);
 #define BAUD_RATE 115200
 
 // Update these with values suitable for your network.
-const char *ssid = "SSID";
-const char *password = "PASSWORD";
+const char* ssid = "SSD";
+const char* password = "PASSWORD";
 
 // Broker address (Raspberry Pi address)
 const char *mqtt_server = "192.168.1.132";
 
 // Publish topic
-const char *publish_topic = "outTopic";
+const char* publish_topic = "NODEMCUDHTHOME01";
 
 // Wi-Fi client
 WiFiClient espClient;
@@ -35,67 +35,67 @@ char msg[100];
 void setup_wifi()
 {
 
-    delay(10);
-    // We start by connecting to a WiFi network
-    WiFi.begin(ssid, password);
+  delay(10);
+  // We start by connecting to a WiFi network
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-    }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
 
-    randomSeed(micros());
+  randomSeed(micros());
 
-    // NodeMCU address
-    // Serial.println(WiFi.localIP());
+  // NodeMCU address
+  // Serial.println(WiFi.localIP());
 }
 
 void reconnect()
 {
-    // Loop until we're reconnected
-    while (!client.connected())
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    // Create a random client ID
+    String clientId = "NODEMCUHOME01-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (!client.connect(clientId.c_str()))
     {
-        // Create a random client ID
-        String clientId = "NODEMCUHOME01-";
-        clientId += String(random(0xffff), HEX);
-        // Attempt to connect
-        if (!client.connect(clientId.c_str()))
-        {
-            // Wait 5 seconds before retrying
-            delay(5000);
-        }
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
+  }
 }
 
 void setup()
 {
-    Serial.begin(BAUD_RATE);
-    // Init DHT
-    DHT.begin();
+  Serial.begin(BAUD_RATE);
+  // Init DHT
+  DHT.begin();
 
-    // Configure Wi-Fi connection
-    setup_wifi();
+  // Configure Wi-Fi connection
+  setup_wifi();
 
-    // Configure broker server
-    client.setServer(mqtt_server, MQTT_PORT);
+  // Configure broker server
+  client.setServer(mqtt_server, MQTT_PORT);
 }
 
 void loop()
 {
-    // Connect the MQTT client
-    if (!client.connected())
-    {
-        reconnect();
-    }
-    client.loop();
+  // Connect the MQTT client
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 
-    // Read temperature in celsius
-    float T = DHT.readTemperature();
-    // Read humidity
-    float H = DHT.readHumidity();
+  // Read temperature in celsius
+  float T = DHT.readTemperature();
+  // Read humidity
+  float H = DHT.readHumidity();
 
-    snprintf(msg, 50, "{\"humidity\": %2.2f, \"temperature\": %2.2f}", H, T);
-    client.publish(publish_topic, msg);
-
-    delay(2000);
+  snprintf(msg, 50, "{\"humidity\": %2.2f, \"temperature\": %2.2f}", H, T);
+  Serial.println(msg);
+  client.publish(publish_topic, msg);
+  delay(2000);
 }

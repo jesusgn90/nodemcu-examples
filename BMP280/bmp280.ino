b315aac20c9e1b2d27286b6d161f4d0474f38869
JORGEGN <jorgegonzaleznovez@hotmail.com>
2020-08-03 23:07:22 +0200

    Enhancement using enum type and switch

--- BMP280/bmp280.ino
+++ BMP280/bmp280.ino
@@ -1,44 +1,66 @@
-#include <Adafruit_BMP280.h> 
- 
+#include <Adafruit_BMP280.h>
+
 // Using 0x77 implies both CSB and SDO are connected to 3V3!
 #define BMP280_I2C_ADDRESS  0x77
 
 // initialize Adafruit BMP280 library
 Adafruit_BMP280  bmp280;
- 
-void setup(void)
+
+typedef enum {Pa, hPa, atm, bar, Torr, mmWS, psi} UNIT; // Available measurement units
+
+UNIT unit = hPa; // Replace with the desired unit
+
+void setup()
 {
   Serial.begin(9600);
   delay(1000);
- 
-  /**
-   * SDA connected to NodeMCU D2 pin, which is GPIO4
-   * SCL connected to NodeMCU D1 pin, which is GPIO5
-   */
+
+  /*
+     SDA connected to NodeMCU D2 pin, which is GPIO4
+     SCL connected to NodeMCU D1 pin, which is GPIO5
+  */
   Wire.begin(4, 5);
-  
+
   // initialize the BMP280 sensor
   while (bmp280.begin(BMP280_I2C_ADDRESS) == 0)
-  {  // connection error or device address wrong!
+  {
+    // connection error or device address wrong!
     Serial.println("Could not initialize sensor, retrying now...");
     delay(1000);
   }
 }
- 
+
 void loop()
 {
-  float pressure_Pa = bmp280.readPressure();             // Print pressure in Pa
-  float pressure_hPa = pressure_Pa / 100.0F;             // Print pressure in hPa
-  //float pressure_atm = pressure_Pa * 0.0000102;          // Print pressure in atm
-  //float pressure_bar = pressure_Pa / 100000;             // Print pressure in bar
-  //float pressure_Torr = pressure_Pa * 0.0075;            // Print pressure in Torr
-  //float pressure_mmWS = pressure_Pa * 0.102;             // Print pressure in mmWS
-  //float pressure_psi = pressure_Pa *0.000145;            // Print pressure in psi
+  float pressure = bmp280.readPressure();  // Defaults Pa
 
-  Serial.print("Pressure = ");
-  Serial.print(pressure_hPa);                             // Replace with the desired unit
-  Serial.println(" hPa");                                 // Replace with the desired unit
+  switch (unit) {
+
+    case hPa:
+      pressure /= 100.0F;
+      break;
+
+    case atm:
+      pressure *= 0.0000102F;
+      break;
+
+    case bar:
+      pressure /= 100000;
+      break;
+
+    case Torr:
+      pressure *= 0.0075;
+      break;
+
+    case mmWS:
+      pressure *= 0.102;
+      break;
+
+    case psi:
+      pressure *= 0.000145;
+      break;
+  }
 
   delay(1000);
+
 }
- 
